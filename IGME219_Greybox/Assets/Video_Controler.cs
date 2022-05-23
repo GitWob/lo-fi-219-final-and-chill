009e742c2d7c9827396a6df9f48d5c7359a122c2
Eshsdd <42783718+Eshsdd@users.noreply.github.com>
2022-05-23 18:46:22 -0500

    It needs to be a Capital F

---
+++ IGME219_Greybox/Assets/Video_Controler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Video_Controler : MonoBehaviour
+{
+    public string url;
+    public VideoPlayer vidplayer;
+    GameObject instructions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    vidplayer = GetComponent<VideoPlayer>();
+    vidplayer.url = url;
+        instructions.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+   {
+       if (Input.anyKey)
+       {
+           Play();
+       }
+       
+   }
+
+   void Play()
+   {
+       vidplayer.Play();
+       vidplayer.isLooping = true;
+        instructions.SetActive(false);
+   }
+}
+
